cb5320f8ebf65abb5e06496ebcb98025b2b3214a
Ali <muhammad.shafique1@ucalgary.ca>
2025-08-27 13:42:53 -0600

    Linked frontend and backend and added some CSS styling

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import {useEffect, useState} from 'react'
+
+//components
+import WorkoutDetails from '../components/WorkoutData'
+
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts') //fetch data from backend and store in reponse
+            const jsonData = await response.json() //convert response to json
+
+            if (response.ok) {
+                setWorkouts(jsonData)
+            }
+        }
+
+        fetchWorkouts()
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
         </div>    
     )
 }
