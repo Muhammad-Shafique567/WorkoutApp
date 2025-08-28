9bc97da1f1c57acc10f8804d56c5b08e9127a94d
Ali <muhammad.shafique1@ucalgary.ca>
2025-08-28 00:40:55 -0600

    Added reducers

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,12 +1,32 @@
 import {createContext} from 'react'
+import {useReducer} from 'react'
 
 export const WorkoutsContext = createContext()
 
+export const workoutsReducer = (state, action) => {
+    switch(action.type) {
+        case 'SET_WORKOUTS':
+            return {
+                workouts: action.payload
+            }
+        case 'CREATE_WORKOUT':
+            return {
+                workouts: [action.payload, ...state.workouts] //add new workout to the beginning of the array (spread operator to copy all the existing workouts)
+            }
+        default:
+            return state   
+    }
+}
+
 //children prop is whatever is inside the provider tags in index.js (in this case it's the entire app)
 export const WorkoutsContextProvider = ({children}) => { 
+    const [state, dispatch] = useReducer(workoutsReducer, { //call reducer function
+        workouts: null
+    })
+
     return (
-        //Since children represents the entire app, everything inside the app will have access to the context
-        <WorkoutsContext.Provider>
+        //Since children represents the entire app, everything inside the app will have access to state and dispatch
+        <WorkoutsContext.Provider value={{state, dispatch}}>
             { children }
         </WorkoutsContext.Provider>
     )
