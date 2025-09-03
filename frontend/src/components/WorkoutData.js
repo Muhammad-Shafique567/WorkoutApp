8eb748f2d9cddef95607214477cd21b6c01d8c86
Ali <muhammad.shafique1@ucalgary.ca>
2025-09-02 20:01:41 -0600

    Improved date formatting

--- frontend/src/components/WorkoutData.js
+++ frontend/src/components/WorkoutData.js
@@ -20,7 +20,7 @@ const WorkoutDetails = ({workout}) => {
             <h4>{workout.title}</h4>
             <p><strong>Weight (kg): </strong>{workout.weight}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
-            <p>{formatDistanceToNow}</p>
+            <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix: true})}</p>
             <span className='material-symbols-outlined' onClick={handleClick}>delete</span>
         </div>
     )
