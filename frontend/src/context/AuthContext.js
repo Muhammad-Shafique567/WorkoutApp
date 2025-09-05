9b1ff1ab60c1873b2a43155cba466d985f9d0ee6
Ali <muhammad.shafique1@ucalgary.ca>
2025-09-04 23:06:54 -0600

    React auth context added

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,30 @@
+import { createContext, useReducer, userReducer } from 'react'
+
+export const AuthContext = createContext()
+
+export const authReducer = (state, action) => {
+    switch(action.type) {
+        case 'LOGIN':
+            return { user: action.payload } //payload is the user data
+        case 'LOGOUT':
+            return {user : null} //reset user to null on logout
+        default:
+            return state //return original state if action type doesn't match
+    }
+}
+
+//provides state value to entire app since it wraps the entire app in index.js
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, { //second arg user: null is the initial state
+        user: null
+    })
+
+    console.log('AuthContext state: ', state)
+
+    //State is the user object   
+    return (
+        <AuthContext.Provider value = {{...state, dispatch}}> 
+            { children }
+        </AuthContext.Provider>
+    )
+}
